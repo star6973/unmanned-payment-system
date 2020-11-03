ae884cff353ec62af457185268fa8c1d04d987b3
Ji Myung Hwa <battlesun99@gmai.com>
2020-11-03 16:14:22 +0900

    commit
    Former-commit-id: bcdc2e98c36f2fcac4f0ebe7c8e143e7004688ad

--- GUI/gui.py
+++ GUI/gui.py
@@ -9,13 +9,24 @@ from PyQt5 import uic
 from PyQt5.uic import loadUi
 import numpy as np
 import time
-import torch
 from mmdet.apis import inference_detector, init_detector
 from mmcv.utils import is_str
 import random
 import mmcv
 
-model = torch.load('../../content/Object_Detection/mmdetection_object_detection_demo/save_model/SSD_model.pt')
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import skimage
+from skimage import io
+from PIL import Image
+import torchvision
+import eval_widerface
+import torchvision_model
+import model
+import os
+
+model = torch.load('../../content/Object_Detection/mmdetection_object_detection_demo/save_model/DynamicRCNN_model.pt')
 score_thr = 0.5
 CLASSES = ('ID_gum', 'buttering', 'couque_coffee', 'chocopie', 'cidar', 
            'couque_white', 'coke', 'diget_ori', 'diget_choco', 'gumi_gumi', 
@@ -27,6 +38,10 @@ class_by_color = [[128, 0, 0], [255,0,0], [255,165,0], [128,128,0], [124,252,0],
                   [147,112,219], [216,191,216], [199,21,133], [255,105,180], [250,235,215], [160,82,45], 
                   [205,133,63], [255,222,173], [112,128,144], [0,0,0]]
 label_list = []
+
+model_path = 'GUI/out/model.pt'
+depth = 50
+scale = 1.0
 
 def random_color(label_num):
     return tuple(class_by_color[label_num])
@@ -106,7 +121,7 @@ def show_result(img,
     ]
     labels = np.concatenate(labels)
 
-    if segm_result is not None and len(labels) > 0:  # non empty
+    if segm_result is not None and len(labels) > 0: # non empty
         segms = mmcv.concat_list(segm_result)
         inds = np.where(bboxes[:, -1] > score_thr)[0]
         np.random.seed(42)
@@ -140,41 +155,42 @@ def show_result(img,
     if not (show or out_file):
         return img, detect_labels
 
-# 객체 인식 화면
+
+# 객체 인식 화면(4)
 class ObjectDetection(QDialog):
     def __init__(self, parent):
         super(ObjectDetection, self).__init__(parent)
-        detection_ui = 'GUI/face_detection.ui'
+        detection_ui = 'GUI/fourth(detection).ui'
         uic.loadUi(detection_ui, self)
-        self.image=None
-        self.processedImage=None
+        self.image = None
+        self.processedImage = None
         self.startButton.clicked.connect(self.start_webcam)
         self.stopButton.clicked.connect(self.stop_webcam)
         self.detectButton.setCheckable(True)
         self.detectButton.toggled.connect(self.detect_webcam_face)
-        self.face_Enabled=False
+        self.face_Enabled = False
         self.show()
 
     def detect_webcam_face(self,status):
         if status:
             self.detectButton.setText('Stop Detection')
-            self.face_Enabled=True
+            self.face_Enabled = True
         else:
             self.detectButton.setText('Detect Face')
-            self.face_Enabled=False
+            self.face_Enabled = False
 
     def start_webcam(self):
-        self.capture=cv2.VideoCapture(0)
+        self.capture = cv2.VideoCapture(0)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 
-        self.timer=QTimer(self)
+        self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(5)
 
     def update_frame(self):
-        ret,self.image=self.capture.read()
-        self.image=cv2.flip(self.image,1)
+        ret, self.image = self.capture.read()
+        self.image=cv2.flip(self.image, 1)
 
         self.displayImage(self.image, 1)
         if(self.face_Enabled):
@@ -218,24 +234,141 @@ class ObjectDetection(QDialog):
                 self.listWidget.addItem(str(CLASSES[label]))
                 label_list.append(label)
 
-# 얼굴 등록 or 결제 화면
+
+# 얼굴 및 정보 등록 화면(3)
+class RegisterInfo(QDialog):
+    def __init__(self, parent):
+        super(RegisterInfo, self).__init__(parent)
+        info_ui = 'GUI/third(info).ui'
+        uic.loadUi(info_ui, self)
+        
+        self.pushButton.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+        self.label_3.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 10))
+        self.label_4.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+        self.label_5.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+        self.label_6.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+        self.label_7.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+
+        self.start_webcam()
+        self.pushButton.clicked.connect(self.getInfo)
+
+        self.show()
+
+    # 등록하기 버튼을 눌렀을 경우, 이름/생년월일/성별/거주지/얼굴 정보 -> 데이터베이스에 저장하기
+    def getInfo(self):
+        name = self.textEdit_2.toPlainText()
+        birth = self.textEdit_3.toPlainText()
+        sex = self.textEdit_4.toPlainText()
+        loc = self.textEdit_5.toPlainText()
+
+        return name, birth, sex, loc
+
+    def start_webcam(self):
+        return_layers = {'layer2':1,'layer3':2,'layer4':3}
+        self.RetinaFace = torchvision_model.create_retinaface(return_layers)
+        retina_dict = self.RetinaFace.state_dict()
+
+        pre_state_dict = torch.load(model_path)
+        pretrained_dict = {k[7:]: v for k, v in pre_state_dict.items() if k[7:] in retina_dict}
+        self.RetinaFace.load_state_dict(pretrained_dict)
+
+        self.RetinaFace = self.RetinaFace.cuda()
+        self.RetinaFace.eval()
+
+        self.capture = cv2.VideoCapture(-1)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(3)
+
+    def update_frame(self):
+        ret, self.image = self.capture.read()
+        self.image = cv2.flip(self.image, 1)
+        self.displayImage(self.image, 1)
+
+        self.image = torch.from_numpy(self.image)
+        self.image = self.image.permute(2,0,1)
+
+        if not scale == 1.0:
+            size1 = int(self.image.shape[1]/scale)
+            size2 = int(self.image.shape[2]/scale)
+            self.image = resize(self.image.float(),(size1,size2))
+
+        # 얼굴 인식
+        detected_image = self.detect_face(self.image)
+        self.displayImage(detected_image, 1)
+  
+    def detect_face(self, img):
+        input_img = img.unsqueeze(0).float().cuda()
+        picked_boxes, picked_landmarks, picked_scores = eval_widerface.get_detections(input_img, self.RetinaFace, score_threshold=0.5, iou_threshold=0.3)
+
+        np_img = img.cpu().permute(1,2,0).numpy()
+        np_img.astype(int)
+        img = np_img.astype(np.uint8)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        for j, boxes in enumerate(picked_boxes):
+            if boxes is not None:
+                for box,landmark,score in zip(boxes,picked_landmarks[j],picked_scores[j]):
+                    cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(0,0,255),thickness=2)
+                    cv2.circle(img,(landmark[0],landmark[1]),radius=1,color=(0,0,255),thickness=2)
+                    cv2.circle(img,(landmark[2],landmark[3]),radius=1,color=(0,255,0),thickness=2)
+                    cv2.circle(img,(landmark[4],landmark[5]),radius=1,color=(255,0,0),thickness=2)
+                    cv2.circle(img,(landmark[6],landmark[7]),radius=1,color=(0,255,255),thickness=2)
+                    cv2.circle(img,(landmark[8],landmark[9]),radius=1,color=(255,255,0),thickness=2)
+                    cv2.putText(img, text=str(score.item())[:5], org=(box[0],box[1]), fontFace=font, fontScale=0.5, thickness=1, lineType=cv2.LINE_AA, color=(255, 255, 255))
+
+        return img
+
+    def displayImage(self, img, window=1):
+        qformat = QImage.Format_Indexed8
+        if len(img.shape)==3:
+            if img.shape[2]==4:
+                qformat=QImage.Format_RGBA8888
+            else:
+                qformat=QImage.Format_RGB888
+
+        outImage = QImage(img,img.shape[1], img.shape[0], img.strides[0], qformat)
+        outImage = outImage.rgbSwapped()
+
+        if window==1:
+            self.face_label.setPixmap(QPixmap.fromImage(outImage))
+            self.face_label.setScaledContents(True)
+
+
+# 얼굴 등록 or 결제 화면(2)
 class RegisterWindow(QDialog):
     def __init__(self, parent):
         super(RegisterWindow, self).__init__(parent)
-        register_ui = 'GUI/register.ui'
+        register_ui = 'GUI/second(register).ui'
         uic.loadUi(register_ui, self)
+
+        self.register_button.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+        self.payment_button.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
+        
+        self.register_button.clicked.connect(self.faceRegisterButtonFunction)
         self.payment_button.clicked.connect(self.paymentButtonFunction)
+        
         self.show()
 
     def paymentButtonFunction(self):
         ObjectDetection(self)
 
-# 시작 화면
+    def faceRegisterButtonFunction(self):
+        RegisterInfo(self)
+
+# 시작 화면(1)
 class MainWindow(QMainWindow) :
     def __init__(self) :
         super().__init__()
-        main_ui = 'GUI/main.ui'
+        main_ui = 'GUI/first(main).ui'
         uic.loadUi(main_ui, self)
+
+        self.title_label.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 30))
+        self.start_button.setFont(QFont("GUI/12롯데마트드림Bold.ttf", 15))
 
         # START 버튼 클릭 시 이벤트 발생
         self.start_button.clicked.connect(self.startButtonFunction)
@@ -248,4 +381,4 @@ if __name__ == "__main__" :
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
-    app.exec_() 
+    app.exec_()
